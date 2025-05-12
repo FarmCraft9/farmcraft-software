a5891590be9f59675aa5c8d4c58bf133ae261422
FarmCraft9 <realconsoletech@hotmail.com>
2025-05-12 20:14:28 +0200

    Add files via upload

---
+++ assets/storage/FieldSimulator.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+
+echo [42mFIELD SIMULATOR 2025[0m
+echo.
+echo Keyboard Controls:
+echo.
+echo Left arrow key + Enter: Button 1
+echo.
+echo Right arrow key + Enter: Button 2
+
+echo Download the latest version at [1mhttps://farmcraft9.github.io/farmcraft-software/[0m
+start assets\FieldSimulator2025.bat
+pause
